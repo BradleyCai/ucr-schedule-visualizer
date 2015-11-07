d223d9192757afe7bc8f6d4b1eafa23c649a0c54
Ammon Smith <ammon.i.smith@gmail.com>
2015-11-07 15:59:41 -0800

    Makefile updates regular expressions in ../js/

---
+++ regex/update_regex.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+from __future__ import with_statement
+import re, sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: update_regex.py field-name input-file output-file")
+        exit(1)
+
+    field = sys.argv[1]
+
+    with open(sys.argv[2], 'r') as fh:
+        to_replace = fh.read()
+
+    with open(sys.argv[3], 'r') as fh:
+        output_text = fh.read()
+
+    to_replace = to_replace.replace("\\n", "\\\\n").strip()
+
+    output_text = \
+        re.sub(r"this\.%s\s*=\s*\/.*\/g;" % re.escape(field), \
+               "this.%s = /%s/g;" % (field, to_replace, \
+               output_text)
+     
+    with open(sys.argv[3], 'w+') as fh:
+        fh.write(output_text)
+
+
